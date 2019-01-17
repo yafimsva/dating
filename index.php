9aa4d36f0705d6d7d3c28cba19fc03ee48be5c84
yafimsva <yafimsva@gmail.com>
2019-01-16 19:18:22 -0500

    basic structure, and fat free installed

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload
+require_once ('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//Turn of Fat-Free error reporting
+$f3->set('DEBUG', 3);
+
+//Define a default route
+$f3->route('GET /', function() {
+
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//Run fat free
+$f3->run();
