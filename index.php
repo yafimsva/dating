65c5530fb6b6823ad4a79b95482ebc2f3b3f4ae9
yafimsva <yafimsva@gmail.com>
2019-02-03 00:25:41 -0500

    final changes

--- index.php
+++ index.php
@@ -13,6 +13,16 @@ require_once ('vendor/autoload.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
+$f3->set('states', array("Alabama", "Alaska", "Arizona", "Arkansas",
+    "California", "Colorado", "Connecticut", "Delaware", "Florida",
+    "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+    "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+    "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+    "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+    "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma",
+    "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia",
+    "Washington", "West Virginia", "Wisconsin", "Wyoming"));
 
 //Turn of Fat-Free error reporting
 $f3->set('DEBUG', 3);
