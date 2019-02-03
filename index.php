a5b43f80699682c789dfec2c44153771574871cd
yafimsva <yafimsva@gmail.com>
2019-02-03 00:00:54 -0500

    fixed summary page

--- index.php
+++ index.php
@@ -24,35 +24,23 @@ require_once('model/validation-functions.php');
 //Define a default route
 $f3->route('GET /', function() {
 
+    $_SESSION = array();
     $view = new View;
     echo $view->render('views/home.html');
 });
 
 //register route
 $f3->route('GET|POST /register', function($f3) {
-    $_SESSION = array();
     $_SESSION['firstName'] = $_POST['firstName'];
     $_SESSION['lastName'] = $_POST['lastName'];
     $_SESSION['age'] = $_POST['age'];
     $_SESSION['gender'] = $_POST['gender'];
     $_SESSION['phone'] = $_POST['phone'];
 
-    if (isset($_SESSION['firstName']))
+    if (($_SESSION['firstName']) != "" )
     {
         $f3->reroute('/profile');
     }
-
-
-//    if(validName($_POST['firstName']) && validName($_POST['lastName']) && validAge($_POST['age']) && validPhone($_POST['phone'])) { #if all inputs are valid
-//        $_SESSION['firstName'] = $_POST['firstName'];
-//        $_SESSION['lastName'] = $_POST['lastName'];
-//        $_SESSION['age'] = $_POST['age'];
-//        $_SESSION['phoneN'] = $_POST['phone'];
-//        $_SESSION['gender'] = $_POST['gender'];
-//        $f3->reroute('/profile');
-//    }
-
-
 
     $template = new Template();
     echo $template->render('views/register.html');
@@ -60,7 +48,6 @@ $f3->route('GET|POST /register', function($f3) {
 
 //profile route
 $f3->route('GET|POST /profile', function($f3) {
-    $template = new Template();
 
     $_SESSION['email'] = $_POST['email'];
     $_SESSION['bib'] = $_POST['bib'];
@@ -74,8 +61,7 @@ $f3->route('GET|POST /profile', function($f3) {
         $f3->reroute('/interests');
     }
 
-    print_r($_SESSION);
-
+    $template = new Template();
     echo $template->render('views/profile.html');
 });
 
@@ -85,18 +71,17 @@ $f3->route('GET|POST /interests', function($f3) {
     $_SESSION['outDoor'] = $_POST['outDoor'];
 
 
-    if (isset($_SESSION['inDoor']) or isset($_SESSION['outDoor']))
+    if (isset($_SESSION['inDoor']) and isset($_SESSION['outDoor']))
     {
         $f3->reroute('/summary');
     }
 
-    print_r($_SESSION);
     $template = new Template();
     echo $template->render('views/interests.html');
 });
 
 //summary route
-$f3->route('GET|POST /summary', function() {
+$f3->route('GET|POST /summary', function($f3) {
 
     $template = new Template();
     echo $template->render('views/summary.html');
