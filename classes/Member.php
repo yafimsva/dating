5f06885b6c9aaf645c5218579a318eb032d5d2d5
yafimsva <yafimsva@gmail.com>
2019-02-14 15:52:51 -0500

    added class Member

---
+++ classes/Member.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yafimvainilovich
+ * Date: 2019-02-14
+ * Time: 12:49
+ */
+
+class Member
+{
+    private $_fname;
+    private $_lname;
+    private $_age;
+    private $_gender;
+    private $_phone;
+    private $_email;
+    private $_state;
+    private $_seeking;
+    private $_bio;
+}
