bb2a8b60a7d9eef4342d3454d82f41bb24afd4e2
yafimsva <yafimsva@gmail.com>
2019-03-02 15:53:20 -0500

    added database class

---
+++ model/Database.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yafimvainilovich
+ * Date: 2019-03-02
+ * Time: 12:49
+ */
+
+class Database
+{
+    public function connect()
+    {
+
+    }
+
+    public function insertMember()
+    {
+
+    }
+
+    public function getMembers()
+    {
+
+    }
+
+    public function getMember()
+    {
+
+    }
+
+
+
+}
